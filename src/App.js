438e17654de715572afe3f1ee88e0bae1c67a0eb
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2018-01-03 10:29:15 -0800

    Add test component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Test from './component/test_component'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <h1>My App</h1>
+        <Test />
       </div>
     );
   }
