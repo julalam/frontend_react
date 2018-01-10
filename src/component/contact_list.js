2e0dce0e22cd1967635634f145480b84fd652ac3
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2018-01-09 16:26:23 -0800

    Show pending requests on user page

--- src/component/contact_list.js
+++ src/component/contact_list.js
@@ -5,14 +5,25 @@ class ContactList extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      users: [],
+      requests: [],
       contacts: [],
     };
   }
 
   componentDidMount() {
     axios.get('http://localhost:8080/users').then((response) => {
-      const contacts = Array.from(response.data);
+      const users = Array.from(response.data);
       this.setState({
+        users: users,
+      })
+    });
+
+    axios.get('http://localhost:8080/contacts?from=' + this.props.session.id).then((response) => {
+      const requests = Array.from(response.data.requests);
+      const contacts = Array.from(response.data.contacts);
+      this.setState({
+        requests: requests,
         contacts: contacts,
       })
     });
@@ -22,15 +33,65 @@ class ContactList extends Component {
     this.props.onContact(contact);
   }
 
+  handleRequest(user, event) {
+    axios.post('http://localhost:8080/contacts', {
+      from: this.props.session.id,
+      to: user.id,
+    }).then((response) => {
+      console.log(`Sent request from ${this.props.session.username} to ${user.username}`);
+    });
+  }
+
+  acceptRequest(event) {
+
+  }
+
+  declineRequest(event) {
+
+  }
+
   render() {
+    const users = this.state.users.map(user => {
+      return (
+        <div key={user.id}>
+          <div>{user.username}</div>
+          <button onClick={this.handleRequest.bind(this, user)} type="button">Send Request</button>
+        </div>
+      )
+    });
+
+    const requests = this.state.requests.map(contact => {
+      return (
+        <div key={contact.sender.id}>
+          <div>{contact.sender.username}</div>
+          <button onClick={this.acceptRequest.bind(this, contact.sender)} type="button">Accept Request</button>
+          <button onClick={this.declineRequest.bind(this, contact.sender)} type="button">Decline Request</button>
+        </div>
+      )
+    });
+
     const contacts = this.state.contacts.map(contact => {
-      return <div onClick={this.handleClick.bind(this, contact)} key={contact.id}>{contact.username}</div>
+      return (
+        <div key={contact.id}>
+          <div onClick={this.handleClick.bind(this, contact)}>{contact.username}</div>
+        </div>
+      )
     });
 
     return (
       <div>
-        <strong>Contact List:</strong>
-        {contacts}
+        <div>
+          <strong>All Users:</strong>
+          {users}
+        </div>
+        <div>
+          <strong>All Pending Requests:</strong>
+          {requests}
+        </div>
+        <div>
+          <strong>Contact List:</strong>
+          {contacts}
+        </div>
       </div>
     );
   }
